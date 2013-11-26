14c87f724b1eba1c893834b079b1ada3a7c57e7a
JrCs <90z7oey02@sneakemail.com>
2013-11-26 14:43:57 +0100

    Add ConfigObj python module (used by pastebinit)

---
+++ bin/test-python-import.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Tools to see if a module can be import
+# Example: test-python-import.py distutils.sysconfig
+
+import sys
+
+try:
+      __import__(sys.argv[1])
+      print "Sucessfully import", sys.argv[1]
+except:
+      print "Error!"
+      sys.exit(4)
+      
+sys.exit(0)
